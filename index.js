de94ce3e128307377c7dbd20858cea5e79c05378
joengugi <joengugi361@gmail.com>
2022-01-26 15:05:48 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,67 @@
+/*var firstName = "Joseph";
+console.log(firstName)
+
+var firstString = "I came " + "I saw";
+console.log(firstString)
+
+var myStr = "This is the first sentence. "
+myStr += "This is the second sentence."
+console.log(myStr)
+
+var newString = "My name is "+ firstName + " and I am okay"
+console.log(newString)
+
+var firstNameL = "";
+var firstNameL = firstName[3];
+console.log(firstNameL)
+
+var lastLetter = firstName[firstName.length-2]
+console.log(lastLetter)
+
+function Words(myNoun, myAdjective, myVerb, myAdverb){
+    var result = "";
+    result += "The " + myAdjective + " " + myNoun + " " + myVerb +" " + "into the store " + myAdverb
+    return result;
+
+}
+console.log(Words("fox", "sly", "jumped","stealthly"))
+
+
+var myArray = [[0,1,2], [3,4,5], [6,7,8], [9,10,11]]
+var myData = myArray[1][1]
+myArray.push(["fun", 23]);
+console.log(myArray[4])
+
+var removedNo = myArray.pop();
+console.log(myArray);
+
+var shiftedNo = myArray.shift();
+console.log(myArray); 
+
+var array1 = [["Joseph", 426],["Winnie", 418], ["Lewis", 417]];
+array1.unshift(["Ian", 410]);
+console.log(array1);
+
+function intro(){
+    console.log("Waruup bitches")
+}
+intro();
+
+function myCalc(a,b,x){
+    console.log((a + b) * x)
+}
+myCalc(10, 5, 2) 
+
+var automobile = "Audi"
+function cars(){
+    var automobile = "Ferrari"
+
+    return automobile;
+}
+console.log(cars());
+console.log(automobile);*/
+
+function numbers(val){
+   return val - 5;
+}
+console.log(numbers(10));
